93d6fa38b7a8a1c5b580d8f9eb16c722d944148c
Isaac Akonkwa <akonkwaushindi@gmail.com>
2025-03-06 16:21:17 +0200

    ...

--- app/auth/signUp.jsx
+++ app/auth/signUp.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <View>
-      <Text>SignIn Screen</Text>
+      <Text>SignUp Screen</Text>
     </View>
   )
 }
